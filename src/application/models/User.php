a5e1c2764da3112beefa8d8f201a5c10a957c18e
ajither <ajitheredacheril@hotmail.com>
2017-01-02 19:23:51 +0530

    bug fixed

--- src/application/models/User.php
+++ src/application/models/User.php
@@ -129,7 +129,7 @@ class User extends Eloquent {
         where('email', $userName)->orWhere('username', $userName)->
         join("user_profile","user.user_id","=","user_profile.user_id")->
             join("user_refferal","user.user_id","=","user_refferal.user_id")->
-        get(array("user.user_id as userid","user.email","user.first_name as firstname","user.last_name as lastname","user.username","user_profile.profile_picture as profilepicture","user_profile.refferal_code as refferalcode","user_profile.phone_number as phonenumber", "user_refferal.refferal_point as refferalpoint","user_refferal.refferal_user_count as refferalusercount"));
+        get(array("user.user_id as userid","user.email","user.first_name as firstname","user.last_name as lastname","user.username","user_profile.profile_picture as profilepicture","user_profile.refferal_code as refferalcode","user_profile.phone_number as phonenumber", "user_refferal.refferal_point as refferalpoint","user_refferal.refferal_users_count as refferalusercount"));
         if (isset($result[0])) {
             return $result[0];
         } else {
